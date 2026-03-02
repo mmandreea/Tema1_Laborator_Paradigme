993e89321106b82578d74f45f953342b54029ef2
Andreea <andreeamatamaria@yahoo.com>
2026-03-02 12:21:23 +0200

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        Student student1 = new Student(3568, "Andreea", "Mata", "C221");
+        student1.afiseaza();
+        System.out.println(student1);
+
+    }
+}
